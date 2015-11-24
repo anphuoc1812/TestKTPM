7539f414abe5519993cb64a865cce7ff9fbee0d7
tuankiet234 <tuankiet234@gmail.com>
2015-11-24 13:29:08 +0700

    Start up

---
+++ Calculator/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Class1
+    {
+        public int Cong(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Nhan(int a, int b)
+        {
+            if (b == 0)
+                return 0;
+
+            int res = 0;
+            for (int i = 0; i < Math.Abs(b); i++)
+            {
+                res += a;
+            }
+
+            if (b < 0)
+                return res * -1;
+            return res;
+        }
+    }
+}
---
+++ Sample.Test/CalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Calculator;
+
+namespace Sample.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Class1 _cal;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cal = new Class1();
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(2, 6, 8)]
+        [TestCase(-1, 9, 8)]
+        public void TestCong(int a, int b, int kq)
+        {
+            Assert.AreEqual(kq, _cal.Cong(a, b));
+        }
+
+        [TestCase(0, 3, 0)]
+        [TestCase(3, 0, 0)]
+        [TestCase(-1, 5, -5)]
+        [TestCase(5, -5, -25)]
+        [TestCase(4, 2, 8)]
+        public void TestNhan(int a, int b, int kq)
+        {
+            Assert.AreEqual(kq, _cal.Nhan(a, b));
+        }
+    }
+}
